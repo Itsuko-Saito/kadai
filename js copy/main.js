270651c7b6eb46cd357addd50c63ebb8d8cdd6ac
Saito <is0808is@gmail.com>
2023-05-03 13:01:49 +0900

    sass 不具合で修正効かず

---
+++ js copy/main.js
@@ -0,0 +1,33 @@
+window.addEventListener('load', function () {
+  var $button = document.querySelector('.toggle-menu-button');
+  var $menu = document.querySelector('.header-site-menu');
+  $button.addEventListener('click', function () {
+    if ($menu.classList.contains('is-show')) {
+      $menu.classList.remove('is-show');
+    }
+    else {
+      $menu.classList.add('is-show');
+    }
+  });
+});
+
+
+
+$(window).scroll(function () {
+  var scrollAnimationElm = document.querySelectorAll('.scroll_up');
+  var scrollAnimationFunc = function () {
+    for (var i = 0; i < scrollAnimationElm.length; i++) {
+      var triggerMargin = 150;
+      if (window.innerHeight > scrollAnimationElm[i].getBoundingClientRect().top + triggerMargin) {
+        scrollAnimationElm[i].classList.add('on');
+      }
+    }
+  }
+  window.addEventListener('load', scrollAnimationFunc);
+  window.addEventListener('scroll', scrollAnimationFunc);
+});
+
+$(".qa dt").on("click", function () {
+  $(this).parent().toggleClass("open");
+  $(this).next().slideToggle();
+});
